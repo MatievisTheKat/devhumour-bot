92d45f8aa8764bdd5c195792c2923bcfb994e42d
matievisthekat <matievisthekat@gmail.com>
2022-11-15 22:40:56 +0200

    store images correctly

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os
-from urllib.parse import urlparse
-import requests
+import sqlite3
+from io import BytesIO
+import urllib.request as request
+
+import imagehash
 import praw
 from PIL import Image
-import imagehash
-import sqlite3
 
-conn = sqlite3.connect("database.db")
+conn = sqlite3.connect("database.sqlite")
 
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS posts (id VARCHAR(10) NOT NULL, image BLOB NOT NULL);")
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS posts (id VARCHAR(10) UNIQUE NOT NULL, image BLOB NOT NULL);")
 
 
 def isImage(url):
@@ -19,12 +20,26 @@ def isImage(url):
         return False
 
 
-def similarity(post1, post2):
-    hash1 = imagehash.average_hash(Image.open(
-        f"./cache/{post1.id}{os.path.splitext(urlparse(post1.url).path)[1]}"))
-    hash2 = imagehash.average_hash(Image.open(
-        f"./cache/{post2.id}{os.path.splitext(urlparse(post2.url).path)[1]}"))
+def similarity(img1, img2):
+    print(img1.size, img2.size)
+    hash1 = imagehash.average_hash(img1)
+    hash2 = imagehash.average_hash(img2)
     return 100 - (((hash1 - hash2)/64)*100)
+
+
+def findPostInDatabasePosts(post, list):
+    for item in list:
+        if item[0] == post.id:
+            return True
+
+    return False
+
+
+def getImgFromURL(url):
+    file = request.urlopen(url)
+    data = file.read()
+
+    return data
 
 
 reddit = praw.Reddit("devhumour-bot")
@@ -32,8 +47,18 @@ rising_iter = reddit.subreddit("ProgrammerHumor").rising(limit=None)
 rising = [_ for _ in rising_iter]
 
 for post in rising:
-    path = f"./cache/{post.id}{os.path.splitext(urlparse(post.url).path)[1]}"
-    if isImage(post.url) and not os.path.exists(path):
-        img = requests.get(post.url).content
-        print(img)
-        cursor.execute("INSERT INTO posts VALUES (?, ?);", (post.id, img))
+    dbposts = cursor.execute("SELECT * FROM posts;").fetchall()
+    print(f"\n\nProcessing '{post.id}'")
+    if isImage(post.url) and not findPostInDatabasePosts(post, dbposts):
+        imgdata = getImgFromURL(post.url)
+        img = Image.open(BytesIO(imgdata))
+
+        for dbpost in dbposts:
+            dbimg = Image.open(BytesIO(dbpost[1]))
+            print(similarity(img, dbimg))
+
+        cursor.execute("INSERT INTO posts VALUES (?, ?);",
+                       (post.id, bytearray(imgdata)))
+
+
+conn.commit()
