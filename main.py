3eae7d8744c9bea2455327d41f769c9bca934886
matievisthekat <matievisthekat@gmail.com>
2022-11-29 21:06:23 +0200

    move logic to its own function so i can use threads later

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import sqlite3
 from io import BytesIO
 import urllib.request as request
+# import threading
 
 from imagehash import average_hash
 import praw
 from PIL import Image
 from datetime import datetime
-
-conn = sqlite3.connect("database.sqlite")
-
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS posts (id VARCHAR(10) UNIQUE NOT NULL, image BLOB NOT NULL);")
-cursor.execute("CREATE TABLE IF NOT EXISTS reposts (original_id VARCHAR(10), copy_id VARCHAR(10))")
 
 
 def isImage(url):
@@ -39,31 +34,66 @@ def getImgFromURL(url):
     return data
 
 
-reddit = praw.Reddit("devhumour-bot")
-rising_iter = reddit.subreddit("ProgrammerHumor").rising(limit=None)
-rising = [_ for _ in rising_iter]
+def checkPosts(list):
+    conn = sqlite3.connect("database.sqlite")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS posts (id VARCHAR(10) UNIQUE NOT NULL, image BLOB NOT NULL, created_utc VARCHAR(20) NOT NULL);")
+    cursor.execute("CREATE TABLE IF NOT EXISTS reposts (original_id VARCHAR(10), copy_id VARCHAR(10))")
+    
+    for post in list:
+        dbposts = cursor.execute("SELECT * FROM posts;").fetchall()
+        if isImage(post.url) and not findPostInDatabasePosts(post, dbposts):
+            print(f"Processing '{post.id}'")
+            imgdata = getImgFromURL(post.url)
+            img = Image.open(BytesIO(imgdata))
 
-for post in rising:
-    dbposts = cursor.execute("SELECT * FROM posts;").fetchall()
-    if isImage(post.url) and not findPostInDatabasePosts(post, dbposts):
-        print(f"Processing '{post.id}'")
-        imgdata = getImgFromURL(post.url)
-        img = Image.open(BytesIO(imgdata))
+            for dbpost in dbposts:
+                dbimg = Image.open(BytesIO(dbpost[1]))
+                sim = similarity(img, dbimg)
+                if (sim > 99):
+                    post_created_at = datetime.fromtimestamp(post.created_utc)
+                    dbpost_created_at = datetime.fromtimestamp(float(dbpost[2]))
 
-        for dbpost in dbposts:
-            dbimg = Image.open(BytesIO(dbpost[1]))
-            sim = similarity(img, dbimg)
-            if (sim > 95):
-                post_created_at = datetime.fromtimestamp(post.created_utc)
-                dbpost_created_at = datetime.fromtimestamp(dbpost.created_utc)
+                    og = post.id if post_created_at < dbpost_created_at else dbpost[0]
+                    copy = post.id if post_created_at > dbpost_created_at else dbpost[0]
 
-                og = post if post_created_at < dbpost_created_at else dbpost
-                copy = post if post_created_at > dbpost_created_at else dbpost
+                    print(f"{sim}: og: {og}, copy: {copy}")
+                    cursor.execute(
+                        "INSERT INTO reposts VALUES (?, ?);", (og, copy))
 
-                print(f"{sim}: og: {og.url}, copy: {copy.url}")
-                cursor.execute("INSERT INTO reposts VALUES (?, ?);", (og.id, copy.id))
+            cursor.execute("INSERT INTO posts VALUES (?, ?, ?);",
+                           (post.id, bytearray(imgdata), post.created_utc))
 
-        cursor.execute("INSERT INTO posts VALUES (?, ?);", (post.id, bytearray(imgdata)))
+    conn.commit()
 
 
-conn.commit()
+if __name__ == "__main__":
+    reddit = praw.Reddit("devhumour-bot")
+    subreddit = reddit.subreddit("ProgrammerHumor")
+    
+    rising = [_ for _ in subreddit.rising(limit=None)]
+    checkPosts(rising)
+    
+    # TODO: create threads for different sorting types
+    # rising = [_ for _ in subreddit.rising(limit=None)]
+    # hot = [_ for _ in subreddit.hot(limit=None)]
+    # top_all = [_ for _ in subreddit.top(limit=None, time_filter="all")]
+    # new = [_ for _ in subreddit.new(limit=None)]
+    
+    # print('got posts')
+    
+    # rising_thread = threading.Thread(target=checkPosts,args=([rising]))
+    # hot_thread = threading.Thread(target=checkPosts, args=([hot]))
+    # top_all_thread = threading.Thread(target=checkPosts, args=([top_all]))
+    # new_thread = threading.Thread(target=checkPosts, args=([new]))
+    
+    # rising_thread.start()
+    # hot_thread.start()
+    # top_all_thread.start()
+    # new_thread.start()
+    
+    # rising_thread.join()
+    # hot_thread.join()
+    # top_all_thread.join()
+    # new_thread.join()
+    
